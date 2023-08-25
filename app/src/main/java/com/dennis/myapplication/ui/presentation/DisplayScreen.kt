319f0846a573c60c7194b92f323b1c88a01e8137
Admin <dennixtrevor@gmail.com>
2023-08-25 10:31:10 +0300

    done

--- app/src/main/java/com/dennis/myapplication/ui/presentation/DisplayScreen.kt
+++ app/src/main/java/com/dennis/myapplication/ui/presentation/DisplayScreen.kt
@@ -1,17 +1,34 @@
 package com.dennis.myapplication.ui.presentation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun DisplayScreen(){
 
-    Column {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+            .background(
+                Color.Black,RoundedCornerShape(
+                bottomStart = 12.dp,
+                bottomEnd = 12.dp
+                ))
+            .size(150.dp)
+    ) {
         CalculationSubScreen()
         CalculationAnswerSubScreen()
     }
@@ -19,7 +36,10 @@ fun DisplayScreen(){
 
 @Composable
 fun CalculationSubScreen(){
-   Row {
+   Row(verticalAlignment = Alignment.CenterVertically,
+       horizontalArrangement = Arrangement.End,
+       modifier = Modifier.fillMaxWidth()
+       .padding(12.dp)) {
        Text(
            text = "1",
            fontSize = 22.sp,
@@ -30,7 +50,10 @@ fun CalculationSubScreen(){
 }
 @Composable
 fun CalculationAnswerSubScreen(){
-    Column{
+    Column(
+        horizontalAlignment = Alignment.End,
+        modifier = Modifier.fillMaxWidth()
+        .padding(12.dp)){
         Text(
             text ="0",
             fontSize = 20.sp,
